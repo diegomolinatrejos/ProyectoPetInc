bbe27bc166f6577ed2f50ebb78510d307def184d
*asalazarlcen* <*asalazarl@ucenfotec.ac.cr*>
2023-10-31 22:03:05 -0600

    Modificación App-Logic y API controllers

---
+++ API/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using DTO.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using App_Logic;
+using Microsoft.AspNetCore.Cors;
+
+namespace API.Controllers
+{
+    [EnableCors("MyCorsPolicy")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+
+    public class AdminController : ControllerBase
+    {
+
+    }
+}
---
+++ API/Controllers/GestorController.cs
@@ -0,0 +1,16 @@
+﻿using DTO.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using App_Logic;
+using Microsoft.AspNetCore.Cors;
+
+namespace API.Controllers
+{
+    [EnableCors("MyCorsPolicy")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class GestorController : ControllerBase
+    {
+
+    }
+}
