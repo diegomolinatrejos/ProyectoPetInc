38f7e0b33022df2ce09a02b9c20e28cceb6341a0
Dexfex19 <decheverriao@ucenfotec.ac.cr>
2023-11-07 21:29:50 -0600

    se agregan los cors en el program.cs del API

--- API/Program.cs
+++ API/Program.cs
@@ -7,6 +7,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// configuracion de CORS
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyCorsPolicy",
+        policy =>
+        {
+            policy.AllowAnyOrigin();// www.mypage.com, www.mypage.net, otros
+            policy.AllowAnyHeader();// application /json, aplication/xml, application/text, application/html
+            policy.AllowAnyMethod();// get, post, put, delete
+        });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,9 +29,21 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors();
 
 app.Run();
