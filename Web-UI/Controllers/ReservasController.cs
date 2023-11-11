ddf52484cbbad586b8da9359ab5f5a668f2d47aa
*asalazarlcen* <*asalazarl@ucenfotec.ac.cr*>
2023-11-11 04:51:48 -0600

    FInal Ale

---
+++ Web-UI/Controllers/ReservasController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_UI.Controllers
+{
+    public class ReservasController : Controller
+    {
+        public IActionResult GestionarReservasCliente()
+        {
+            return View();
+        }
+
+        public IActionResult GestionarReservasGestor()
+        {
+            return View();
+        }
+
+        public IActionResult RealizarReservaGestor()
+        {
+            return View();
+        }
+
+        public IActionResult RealizarReserva()
+        {
+            return View();
+        }
+    }
+}
