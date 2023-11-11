5a8b3bbdafa409f456e7ad1fcd8efaa4bd3b515e
Diego Molina Trejos <dmolinat@ucenfotec.ac.cr>
2023-11-10 22:09:51 -0600

    Pantalla de prueba para reportes

---
+++ Web-UI/wwwroot/js/pages/reportesAdm/ReporteAdm.js
@@ -0,0 +1,19 @@
+﻿
+// Información en formato JSON
+const data = [
+    {
+        "tipomascota": "perro",
+        "porcentajeReservas": 40,
+        "montoPorMascota": 240000
+    },
+    {
+        "tipomascota": "gatos",
+        "porcentajeReservas": 50,
+        "montoPorMascota": 500000
+    },
+    {
+        "tipomascota": "Hamster",
+        "porcentajeReservas": 10,
+        "montoPorMascota": 80000
+    }
+];
