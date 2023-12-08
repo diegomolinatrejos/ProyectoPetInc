7bd8ec82b1c282f7abf863248d1da1511bf2f239
Dexfex19 <decheverriao@ucenfotec.ac.cr>
2023-12-07 20:50:22 -0600

    Backend de precioImpuesto, api funcional.

---
+++ App-Logic/AdminPrecioImpuesto.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Crud;
+using DTO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace App_Logic.Admins
+{
+    public class AdminPrecioImpuesto
+    {
+        public List<PrecioImpuesto> GetPrecioImpuesto()
+        {
+            PrecioImpuestoCrud piCrud = new PrecioImpuestoCrud();
+            return piCrud.RetrieveAll<PrecioImpuesto>();
+        }
+
+        public void UpdatePrecioImpuesto(PrecioImpuesto precioImpuesto)
+        {
+            PrecioImpuestoCrud piCrud = new PrecioImpuestoCrud();
+            piCrud.Update(precioImpuesto);
+        }
+    }
+}
---
+++ DataAccess/Crud/PrecioImpuestoCrud.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Dao;
+using DataAccess.Mapper;
+using DTO.Models;
+
+namespace DataAccess.Crud
+{
+    public class PrecioImpuestoCrud : CrudFactory
+    {
+        private PrecioImpuestoMapper piMapper;
+
+        public PrecioImpuestoCrud() : base()
+        {
+            piMapper = new PrecioImpuestoMapper();
+            dao=SqlDao.GetInstance();
+        }
+
+        public override void Create(BaseClass entityDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(BaseClass entityDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<T> RetrieveAll<T>()
+        {
+            List<T> lstResults = new List<T>();
+            SqlOperation operation = piMapper.RetrieveAllStatement();
+
+            List<Dictionary<string, object>> dataResults = dao.ExecuteStoredProcedureWithQuery(operation);
+
+            if (dataResults.Count > 0)
+            {
+                var dtoObjects = piMapper.BuildObjects(dataResults);
+
+                foreach (var ob in dtoObjects)
+                {
+                    lstResults.Add((T)Convert.ChangeType(ob, typeof(T)));
+                }
+            }
+            return lstResults;
+        }
+
+        public override T RetrieveByEmail<T>(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T RetrieveById<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(BaseClass entityDTO)
+        {
+            SqlOperation operation = piMapper.GetUpdateStatement(entityDTO);
+            dao.ExecuteStoredProcedure(operation);
+        }
+    }
+}
